c71365841c984366618980b57a670bf5e002f666
Boris Joffe <boris@joff3.com>
2015-08-22 01:56:03 -0700

    don't hardcode resolution for dualmon

--- dualmon.sh
+++ dualmon.sh
@@ -19,14 +19,13 @@ fi
 if [ "$(echo "$1"|grep "on")" ]
 then
 	echo "Turning VGA1 on..."
-	$XRANDRCMD --output VGA1 --auto
-	echo "Setting multiple monitor settings..."
+	$XRANDRCMD --output VGA1 --right-of LVDS1 --auto
+	#echo "Setting multiple monitor settings..."
 	#$XRANDRCMD --output VGA1 --right-of LVDS1 --output LVDS1 --mode 1280x800
-	#$XRANDRCMD --output VGA1 --left-of LVDS1 --output LVDS1 
-	$XRANDRCMD --output VGA1 --right-of LVDS1 --mode 1280x1024
+	#$XRANDRCMD --output VGA1 --right-of LVDS1 --mode 1280x1024
 	echo "Done."
 #	first arg does not contain 'on' (from above, elif) and does not contain 'off'
-elif [ -z "$(echo "$1"|grep "off")" ] 
+elif [ -z "$(echo "$1"|grep "off")" ]
 then
 	echo "Error. First arg needs to be 'on' or 'off'"
 fi
