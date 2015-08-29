6704de42fbb81e82afd876e24f3792fb5c77965e
Boris Joffe <boris@joff3.com>
2015-08-28 23:12:14 -0700

    dont need sudo for suspend

--- suspend.sh
+++ suspend.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo echo "Suspending..." # to ask for sudo password immediately
-xautolock -detectsleep -locker slock -locknow; sleep 2 && sudo systemctl suspend
+echo "Suspending..."
+xautolock -locknow
+sleep 2
+systemctl suspend
