917b512086f7c4c8d2006dd709fdce8f7dd92fe0
Boris Joffe <boris@joff3.com>
2015-09-28 20:41:17 -0700

    fix troubleshooting script to work properly

--- fix.sh
+++ fix.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
-CHECK='pgrep xautolock'
-if [[ $($CHECK) ]] ; then
+if [[ $(pgrep xautolock) ]] ; then
 	echo "[OK] xautolock is running"
 else
 	echo -e "[Error] xautolock is not running.\nStarting..."
 	sleep 1
 	xautolock-init.sh
-	[[ $($CHECK) ]] && echo "[FIXED] xautolock started" || "[FAIL] xautolock failed to start"
+	[[ $(pgrep xautolock) ]] && echo "[FIXED] xautolock started" || "[FAIL] xautolock failed to start"
 fi
 
-CHECK='localectl status | head -1 | tail -1 | grep dvorak'
-if [[ $(bash -c $CHECK) ]] ; then
+if [[ $(localectl status | grep dvorak) ]] ; then
 	echo "[OK] Keyboard is set to: dvorak"
 else
 	echo -e "\n[Error] The keyboard layout is not dvorak"
 	setxkbmap dvovark
-	[[ $(bash -c $CHECK) ]] && echo "[FIXED] Set keyboard layout to: dvorak" || "[FAIL] Could not set keyboard to: dvorak"
+	[[ $(localectl status | grep dvorak) ]] && echo "[FIXED] Set keyboard layout to: dvorak" || "[FAIL] Could not set keyboard to: dvorak"
 fi
 
-CHECK='xmodmap -pke | grep " 66" | grep 8'
-if [[ $(bash -c $CHECK) ]] ; then
+if [[ $(xmodmap -pke | grep " 66" | grep 8) ]] ; then
 	echo "[OK] Eight key enabled"
 else
 	echo -e "\n[Error] Eight key not enabled. Enabling eight key..."
 	enable-eight-key.sh
-	[[ $(bash -c $CHECK) ]] && echo "[FIXED] Enabled eight key" || echo "[FAIL] Could not enable eight key"
+	[[ $(xmodmap -pke | grep " 66" | grep 8) ]] && echo "[FIXED] Enabled eight key" || echo "[FAIL] Could not enable eight key"
 fi
 
-CHECK='$SOLARIZED'
-if [[ $CHECK == 1 ]] ; then
+if [[ $SOLARIZED > 0 ]] ; then
 	echo "[OK] SOLARIZED is set to: $SOLARIZED"
 else
 	echo -e "\n[Error] SOLARIZED variable is not set.\nSet SOLARIZED manually or by sourcing '.env'..."
