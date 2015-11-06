7a323dde66c11bfccd806e37b8627552fa6264fa
Boris Joffe <boris@joff3.com>
2015-11-06 11:38:12 -0800

    xautolock sometimes doesn't work when suspending so kill any running xautolock process in xautolock-init

--- xautolock-init.sh
+++ xautolock-init.sh
@@ -4,7 +4,17 @@
 # notify - seconds to turn off screen
 # killtime - minutes after locking before suspending
 
+PID=`pidof xautolock`
+if [[ $PID -ne 0 ]]; then
+	echo "Process running...killing"
+	kill -15 $PID
+else
+	echo "Process not running...starting xautolock"
+fi
+
 exec xautolock -detectsleep -nowlocker slock \
 		-time 10 -locker slock \
 		-notify 5 -notifier "xset dpms force off" \
 		-killtime 20 -killer "systemctl suspend" &
+
+echo "[DONE] xautolock started"
