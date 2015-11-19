b42a3ef469b5f579f1da312102f6fae04796ac03
Boris Joffe <boris@joff3.com>
2015-11-18 22:23:37 -0800

    make suspend.sh restart xautolock automatically

--- suspend.sh
+++ suspend.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 echo "Suspending..."
 
-xautolock -locknow
-SUCCESSFUL_LOCK=$?
+locknow() {
+	xautolock -locknow
+	echo $?
+}
 
-if [ $SUCCESSFUL_LOCK == 0 ]; then
+suspendnow() {
 	sleep 1
 	systemctl suspend
+}
+
+if [[ `locknow` == 0 ]]; then
+	suspendnow
 else
-	notify.sh "could not lock screen...start xautolock"
+	notify.sh "Could not lock screen...will restart xautolock and try again"
+	xautolock-init.sh
+	sleep 2
+	[[ `locknow` == 0 ]] && suspendnow || notify.sh "Still couldn't lock screen...try to do it manually"
 fi
