fa5d68777738aa47d91934f21efd2e07019b84b3
Boris Joffe <boris@joff3.com>
2015-12-19 18:21:40 -0800

    add presentmode.sh

---
+++ presentmode.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+help='presentmode.sh [on|off]'
+usage.sh $help $@ && exit
+
+case $1 in
+'on')
+	redshift -x
+	pkill redshift
+	pkill workrave
+	;;
+'off')
+	nohup redshift -o &
+	nohup workrave &
+	;;
+*)
+	echo "USAGE: $help"
+esac
+
