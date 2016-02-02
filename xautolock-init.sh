f2beef318eda5798e035b400df11cc47e2e2d4d8
Boris Joffe <boris@joff3.com>
2016-02-01 16:22:43 -0800

    update xautolock - tried to get physlock to work

--- xautolock-init.sh
+++ xautolock-init.sh
@@ -4,17 +4,31 @@
 # notify - seconds to turn off screen
 # killtime - minutes after locking before suspending
 
-PID=`pidof xautolock`
-if [[ $PID -ne 0 ]]; then
+set -u
+
+# Suspend doesn't work with physlock
+#readonly locker="physlock -d -s"
+readonly locker="slock"
+
+pid=`pidof xautolock`
+if [[ "$pid" -ne 0 ]]; then
 	echo "Process running...killing"
-	kill -15 $PID
+	kill -15 $pid
+
+	sleep 2
+	pid=`pidof xautolock`
+	if [[ "$pid" -ne 0 ]]; then
+		echo "Could not kill xautolock. Force-killing"
+		kill -9 $pid
+	fi
 else
 	echo "Process not running...starting xautolock"
 fi
 
-xautolock -detectsleep -nowlocker slock \
-	-time 10 -locker slock \
+xautolock -detectsleep -nowlocker "$locker" \
+	-time 10 -locker "$locker" \
 	-notify 5 -notifier "xset dpms force off" \
 	-killtime 20 -killer "systemctl suspend"
+#    -secure
 
 echo "[DONE] xautolock started"
