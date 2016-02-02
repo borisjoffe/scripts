7683e6f7765a38dfc3dbeb6665e8bc5428d15159
Boris Joffe <boris@joff3.com>
2016-02-02 15:55:20 -0800

    update distro to use bash, fix ubuntu identification later

--- distro.sh
+++ distro.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ $1 =~ "help" ]]; then
 	echo 'First argument (TYPE) can be "id" (default), "name", "pretty_name", etc"'
@@ -10,4 +10,6 @@ TYPE=${1:-"ID"}
 # convert to uppercase
 TYPE=`echo $TYPE | tr '[:lower:]' '[:upper:]'`
 
+# TODO: doesn't work for Ubuntu
+# fields are in the form DISTRO_<FIELD>=
 grep "^"$TYPE /etc/*release | awk -F= '{print $2}' | sed 's/"//g'
