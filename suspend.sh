ce1686672022f2b9498095d87f76cb202c5275e1
Boris Joffe <boris@joff3.com>
2016-02-01 16:18:40 -0800

    suspend - add support for disk suspend (hibernate) and hybrid suspend

--- suspend.sh
+++ suspend.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-echo "Suspending..."
+
+# Suspends to ram (default), disk, or both
+# Deps: usage.sh, notify.sh
+
+set -u
+
+usage.sh "suspend.sh [ram|disk|hybrid]" $@ && exit
+
+readonly input=${1:-ram}
+
+case $input in
+	ram|mem|memory|suspend)
+		cmd="suspend"
+		msg="Suspending"
+		;;
+	disk|hibernate)
+		cmd="hibernate"
+		msg="Hibernating"
+		;;
+	hybrid)
+		cmd="hibernate"
+		msg="Hybrid suspend/hibernate"
+		;;
+	*)
+		suspend.sh ram
+		;;
+esac
+
+notify.sh "$msg..."
 
 locknow() {
 	xautolock -locknow
@@ -8,14 +36,14 @@ locknow() {
 
 suspendnow() {
 	sleep 1
-	systemctl suspend
+	systemctl "$cmd"
 }
 
-if [[ `locknow` == 0 ]]; then
+if [[ $(locknow) == 0 ]]; then
 	suspendnow
 else
 	notify.sh "Could not lock screen...will restart xautolock and try again"
 	run xautolock-init.sh
 	sleep 2
-	[[ `locknow` == 0 ]] && suspendnow || notify.sh "Still couldn't lock screen...try to do it manually"
+	[[ $(locknow) == 0 ]] && suspendnow || notify.sh "Still couldn't lock screen...try to do it manually"
 fi
