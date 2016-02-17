46d64c7202ebd0c3cac872ea5de26382672ca683
Boris Joffe <boris@joff3.com>
2016-02-17 09:29:27 -0800

    slightly improved lastmod - allows multiple args to lma (lastmod.sh -A alias)

--- lastmod.sh
+++ lastmod.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Deps: usage, all-but-last-elements
+
+USAGE="lm [0 or more args to ls] [path] [number of files display] \n\
+argument priority is: path, number of files, ls args"
+usage.sh "$USAGE" $@ && exit
+
 if [[ $# > 2 ]]; then
-	LS_ARGS=$1
-	shift
+	LS_ARGS=`all-but-last-elements $@`
+	shift $(($#-2))
 fi
 
 NUM_FILES=${2:-5}
