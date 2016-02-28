a9f9d15b8ecbf042f3feeea2bef141d586ef8e1b
Boris Joffe <boris@joff3.com>
2016-02-28 11:42:12 -0800

    spaceToKebabRename - print what was renamed

--- spaceToKebabRename.sh
+++ spaceToKebabRename.sh
@@ -3,3 +3,4 @@
 # on Debian/Ubuntu systems, it's just called "rename" and is installed by default
 # Arch requires perl-rename to be installed separately
 perl-rename 's/ /-/g' $@
+perl-rename -n 's/ /-/g' $@ # print rename
