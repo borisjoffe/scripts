fe9455ae0383b03a0127ab6701e3d699e9ec1e42
Boris Joffe <boris@joff3.com>
2016-12-16 14:33:51 -0800

    locknow - show failure message, minor updates

--- locknow.sh
+++ locknow.sh
@@ -2,10 +2,26 @@
 
 set -u
 
-xautolock -locknow
-if [[ $? != 0 ]]; then
+# xautolock -locknow
+# st=$?
+# notify.sh "xautolock status: $st"
+
+if ! xautolock -locknow; then
+	notify.sh "Failed initial lock"
+	notify.sh "Restarting xautolock"
+	xautolock-init.sh
+	sleep 1
+	#wait $! # doesn't work - fix this
+	xautolock -locknow
+else
+	notify.sh "success lock: $?"
+fi
+exit
+
+# xautolock -locknow
+if xautolock -locknow; then
 	notify.sh "Starting xautolock"
-	xautolock-init.sh &
+	xautolock-init.sh
 	sleep 1
 	#wait $! # doesn't work - fix this
 	xautolock -locknow
